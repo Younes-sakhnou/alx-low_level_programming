16a11c4eb4c44015d9eeceb6564bb09a8ac74b31
younesapi <younes.sh1234@gmail.com>
2023-03-22 01:03:54 +0000

    commited

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - pritns the sign of a number
+ *
+ * @n: we will check this number sign
+ *
+ * Return: 1 if n > 0 and 0 if n == 0 and -1 if n < 0
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+
+		_putchar('+');
+	return (1);
+	}
+
+	else if (n < 0)
+	{
+
+		_putchar('-');
+	return (-1);
+	}
+
+	else
+	{
+
+		_putchar('0');
+	return (0);
+	}
+}
