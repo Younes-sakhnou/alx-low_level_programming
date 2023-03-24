2bdeb8788c25b9d507e1a6697147f2f86d77ab32
younesapi <younes.sh1234@gmail.com>
2023-03-24 17:41:23 +0000

    commited

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * mul - multiply two int
+ * @a: multiply a by b
+ * @b: multiply b by a
+ * Return: 0 always (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
