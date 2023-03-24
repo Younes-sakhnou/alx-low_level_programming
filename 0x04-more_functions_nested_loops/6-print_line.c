bc2b620d9f71b0575aaf7863bdd3f4052771553e
younesapi <younes.sh1234@gmail.com>
2023-03-24 23:52:59 +0000

    commited

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line
+ * @n: parametre
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
