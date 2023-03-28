6725955fc261148fc60971701207f988e16229f0
younesapi <younes.sh1234@gmail.com>
2023-03-28 17:45:44 +0000

    commited

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string.
+ * @s: parameter
+ * Return: Void
+ */
+void rev_string(char *s)
+{
+	char c;
+	int i, j, k;
+
+	j = 0;
+	k = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	k = j - 1;
+
+	for (i = 0; i < j / 2; i++)
+	{
+		c = s[i];
+		s[i] = s[k];
+		s[k--] = c;
+	}
+}
