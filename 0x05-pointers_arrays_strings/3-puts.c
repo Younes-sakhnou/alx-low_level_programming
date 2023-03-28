48d85a097a5fe1dcb71f741f8c61df232085059f
younesapi <younes.sh1234@gmail.com>
2023-03-28 01:46:47 +0000

    commited

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - pritns string followed by a new line
+ * @str: string pointer
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
