c59bb917f7c457438a919665fea3ea4a1edc05a7
younesapi <younes.sh1234@gmail.com>
2023-03-29 01:14:26 +0000

    commited

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half -  prints second half of a string, followed by a new line.
+ * @str: parameter.
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int i, middle;
+
+	middle = strlen(str) / 2;
+	if (strlen(str) % 2 != 0)
+		middle++;
+	for (i = middle; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
