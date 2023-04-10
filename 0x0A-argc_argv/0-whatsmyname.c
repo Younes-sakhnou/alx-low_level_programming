19571bbc4431dfa0f7b314d064d037e7d6fcbee6
younesapi <younes.sh1234@gmail.com>
2023-04-10 18:18:06 +0000

    his name, printed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+In this project i'm going to learn how to use arguments passed in my program (argc, argv)
