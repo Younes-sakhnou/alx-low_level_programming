3f5f582e3c4f59e16a373bb80d07ab41cc762150
younesapi <younes.sh1234@gmail.com>
2023-04-17 20:30:23 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,6 +4,12 @@
 
 typedef struct dog dog_t;
 
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
