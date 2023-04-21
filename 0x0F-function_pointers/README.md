f4eab05ce57ee93b28b22c29d0a039c2a2dd9acc
younesapi <younes.sh1234@gmail.com>
2023-04-21 17:54:48 +0000

    pointed else

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+In this project i've learned what are function pointers.
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,20 @@
+#ifndef _HEADER_
+#define _HEADER_
+#include <stddef.h> 
+#include <stdio.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
