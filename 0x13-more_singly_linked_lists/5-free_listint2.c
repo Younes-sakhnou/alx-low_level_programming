e09f05c93d9ae5abcf62c02b79d14b67b7d6b1e6
younesapi <younes.sh1234@gmail.com>
2023-05-02 15:11:10 +0000

    list free, and head set to NULL.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *next;
 
-	if (head == NULL)
-		return;
-
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		next = (*head)->next;
-		free(*head);
-		*head = next;
+		while (*head != NULL)
+		{
+			next = (*head)->next;
+			free(*head);
+			*head = next;
+		}
 	}
-	*head = NULL;
 }
