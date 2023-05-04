0db573ca136e0a301f72715a3b80d6114c7de736
younesapi <younes.sh1234@gmail.com>
2023-05-04 23:24:10 +0000

    convert to binary.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number.
+ * @n: number to be converted.
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int number;
+
+	for (i = 63; i >= 0; i--)
+	{
+		number = n >> i;
+
+		if (number & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
